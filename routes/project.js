e20dda7dd88df8ab8a0fdc6af912070ac420606e
PioterAndrzejewski <109315248+PioterAndrzejewski@users.noreply.github.com>
2023-01-27 00:39:38 +0100

    handle adding team member

--- routes/project.js
+++ routes/project.js
@@ -1,37 +1,36 @@
 const express = require('express');
 const router = express.Router();
 const {readFile, writeFile,  mkdir, copyFile} = require('fs').promises;
+const {join} = require("path");
 
-const PROJECTS_LIST_FILE = './data/projectsList.txt'
+const PROJECTS_LIST_FILE = './data/projectsList.json'
 
 const checkProjectId = async (requestedId) => {
     const projectsListDataFromFile = await readFile(PROJECTS_LIST_FILE, 'utf8');
     const projectsList = JSON.parse(projectsListDataFromFile);
 
     if (projectsList.some(project => project.projectId === requestedId)) {
-        return {projectExists: true, message: "Project exists"};
+        return {projectDoesntExist: false, message: "Project exists"};
     }
-    return {projectExists: false, message: "There is no project under such ID"};
+    return {projectDoesntExist: true, message: "There is no project under such ID"};
 }
 
 router.get('/:id', async (req, res) => {
     const requestedId = parseInt(req.params.id);
     let requestValidation = await checkProjectId(requestedId);
-    console.log(requestValidation)
-    if (!requestValidation.projectExists) {
+    if (requestValidation.projectDoesntExist) {
         const response = JSON.stringify({
             projectExists: requestValidation.projectExists,
-            redirect: "/",
             message: requestValidation.message,
         });
         res.end(response)
         return;
     }
 
-    const response = JSON.stringify({
 
+    res.sendFile(`/${requestedId}/data.json`, {
+        root: join(__dirname, '../data')
     })
-    res.end(response)
 });
 
 
